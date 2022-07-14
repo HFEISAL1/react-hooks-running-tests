72ef67d77d72a65adfbd27038cf81a0bfc6dd1f0
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-14 12:31:40 -0700

    runningtests

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from "./Article";
+import Comment from "./Comment";
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+  return (
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,12 @@
 import React from "react";
-
-function Article() {
-  return (
-    <div>
-      please pass this test
-    </div>
-  );
+class Article extends React.Component {
+  render() {
+    return <div>please pass this test</div>;
+  }
 }
+// returns these DOM elements:
+// <div>
+//  <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+// </div>
 
 export default Article;
